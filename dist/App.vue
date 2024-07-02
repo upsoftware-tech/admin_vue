555d44a757c3e80630a2bd6942650a2214cfafab
4multimedia <info@upsoftware.tech>
2024-07-02 13:54:10 +0200

    1.0.9

---
+++ dist/App.vue
@@ -0,0 +1,9 @@
+<template>
+	<usoft-form>
+		<usoft-field-text label="Adres e-mail" />
+	</usoft-form>
+</template>
+
+<script setup>
+import {UsoftForm, UsoftFieldText} from "./index.js";
+</script>
